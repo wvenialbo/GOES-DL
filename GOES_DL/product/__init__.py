d80c582d3d632b125d87b401b69d0ee2ab6146a7
wvenialbo <wvenialbo@gmail.com>
2024-03-25 09:11:39 -0400

    feat(product): bring product classes to subpackage

--- GOES_DL/product/__init__.py
+++ GOES_DL/product/__init__.py
@@ -1,3 +1,16 @@
 from .product import GOESProduct as GOESProduct
+from .rs_cmip_product import GOESRSCMIProduct as GOESRSCMIProduct
+from .rs_imager_product import GOESRSImagerProduct as GOESRSImagerProduct
+from .rs_lightning_product import (
+    GOESRSLightningProduct as GOESRSLightningProduct,
+)
+from .rs_mcmip_product import GOESRSMCMIProduct as GOESRSMCMIProduct
+from .rs_multi_band_product import (
+    GOESRSMultiBandProduct as GOESRSMultiBandProduct,
+)
+from .rs_product import GOESRSProduct as GOESRSProduct
+from .rs_single_band_product import (
+    GOESRSSingleBandProduct as GOESRSSingleBandProduct,
+)
 from .sg_imager_product import GOES2GImagerProduct as GOES2GImagerProduct
 from .sg_product import GOES2GProduct as GOES2GProduct
