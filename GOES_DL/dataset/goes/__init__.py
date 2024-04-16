aa3b2559f9650c93cd418b60ec318f593135d736
wvenialbo <wvenialbo@gmail.com>
2024-04-16 08:39:30 -0400

    refactor(product): move lightning product class
    Move lightning product classes to its own module file.

--- GOES_DL/dataset/goes/__init__.py
+++ GOES_DL/dataset/goes/__init__.py
@@ -3,9 +3,10 @@ from .locator_derived import (
     GOESProductLocatorDerived as GOESProductLocatorDerived,
 )
 from .locator_glm import GOESProductLocatorGLM as GOESProductLocatorGLM
-from .locator_glm import GOESProductLocatorLCFA as GOESProductLocatorLCFA
+from .locator_lp import GOESProductLocatorLCFA as GOESProductLocatorLCFA
 from .locator_pp import GOESProductLocatorCMIP as GOESProductLocatorCMIP
 from .locator_pp import GOESProductLocatorDMW as GOESProductLocatorDMW
+from .locator_pp import GOESProductLocatorDMWV as GOESProductLocatorDMWV
 from .locator_pp import GOESProductLocatorRad as GOESProductLocatorRad
 from .locator_primary import (
     GOESProductLocatorPrimary as GOESProductLocatorPrimary,
