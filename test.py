5be51946179cf242e08a4dabe11116087f8090de
wvenialbo <wvenialbo@gmail.com>
2024-10-19 03:19:12 -0300

    refactor: update basic tests for changes

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from typing import Any
 
-from GOES_DL.dataset.goes import GOESProductLocatorABIPP as ProductLocatorGOES
-from GOES_DL.dataset.gridsat import GridSatProductLocatorB1 as ProductLocatorB1
-from GOES_DL.datasource import DatasourceAWS as Datasource
-from GOES_DL.downloader import Downloader
+from .GOES_DL.dataset.goes import GOESProductLocatorABIPP as ProductLocatorGOES
+from .GOES_DL.dataset.gridsat import (
+    GridSatProductLocatorB1 as ProductLocatorB1,
+)
+from .GOES_DL.datasource import DatasourceAWS, DatasourceHTTP
+from .GOES_DL.downloader import Downloader
 
 DATE_FORMAT = "%Y-%m-%dT%H:%M%z"
 
@@ -19,9 +21,22 @@ def test(dl: Downloader, start: str, end: str = "") -> list[Any]:
     return files
 
 
-def test_gridsat() -> None:
+def test_gridsat_aws() -> None:
     pd = ProductLocatorB1()
-    ds = Datasource(pd)
+    ds = DatasourceAWS(pd)
+    dl = Downloader(datasource=ds, locator=pd, date_format=DATE_FORMAT)
+
+    test(dl, "1980-01-01T00:00+0000")
+    test(dl, "1980-01-01T00:00+0000", "1980-01-01T06:00+0000")
+    test(dl, "1980-08-23T00:00+0000")
+    test(dl, "1980-08-23T00:00+0000", "1980-08-23T06:00+0000")
+    test(dl, "1980-08-23T00:00+0000", "1980-08-24T00:00+0000")
+    test(dl, "2024-08-23T00:00+0000")
+
+
+def test_gridsat_http() -> None:
+    pd = ProductLocatorB1()
+    ds = DatasourceHTTP(pd)
     dl = Downloader(datasource=ds, locator=pd, date_format=DATE_FORMAT)
 
     test(dl, "1980-01-01T00:00+0000")
@@ -34,14 +49,15 @@ def test_gridsat() -> None:
 
 def test_goes() -> None:
     pd = ProductLocatorGOES("CMIP", "F", "C13", "G16")
-    ds = Datasource(pd.get_base_url("AWS"))
+    ds = DatasourceAWS(pd.get_base_url("AWS"))
     dl = Downloader(datasource=ds, locator=pd, date_format=DATE_FORMAT)
 
     test(dl, "2024-08-23T00:00+0000")
 
 
 def main() -> None:
-    test_gridsat()
+    test_gridsat_aws()
+    test_gridsat_http()
     test_goes()
 
 
