7adb2cd345aa325df83094dd8fc8881648ac651f
wvenialbo <wvenialbo@gmail.com>
2024-10-19 05:52:20 -0300

    feat(tests): add mini test for new datasources

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from typing import Any
 
-from .GOES_DL.dataset.goes import GOESProductLocatorABIPP as ProductLocatorGOES
-from .GOES_DL.dataset.gridsat import (
+from GOES_DL.dataset.goes import GOESProductLocatorABIPP as ProductLocatorGOES
+from GOES_DL.dataset.gridsat import (
     GridSatProductLocatorB1 as ProductLocatorB1,
 )
-from .GOES_DL.datasource import DatasourceAWS, DatasourceHTTP
-from .GOES_DL.downloader import Downloader
+from GOES_DL.dataset.gridsat import (
+    GridSatProductLocatorGC as ProductLocatorGC,
+)
+from GOES_DL.datasource import DatasourceAWS, DatasourceHTTP
+from GOES_DL.downloader import Downloader
 
 DATE_FORMAT = "%Y-%m-%dT%H:%M%z"
 
@@ -27,11 +30,6 @@ def test_gridsat_aws() -> None:
     dl = Downloader(datasource=ds, locator=pd, date_format=DATE_FORMAT)
 
     test(dl, "1980-01-01T00:00+0000")
-    test(dl, "1980-01-01T00:00+0000", "1980-01-01T06:00+0000")
-    test(dl, "1980-08-23T00:00+0000")
-    test(dl, "1980-08-23T00:00+0000", "1980-08-23T06:00+0000")
-    test(dl, "1980-08-23T00:00+0000", "1980-08-24T00:00+0000")
-    test(dl, "2024-08-23T00:00+0000")
 
 
 def test_gridsat_http() -> None:
@@ -39,12 +37,15 @@ def test_gridsat_http() -> None:
     ds = DatasourceHTTP(pd)
     dl = Downloader(datasource=ds, locator=pd, date_format=DATE_FORMAT)
 
-    test(dl, "1980-01-01T00:00+0000")
-    test(dl, "1980-01-01T00:00+0000", "1980-01-01T06:00+0000")
-    test(dl, "1980-08-23T00:00+0000")
-    test(dl, "1980-08-23T00:00+0000", "1980-08-23T06:00+0000")
-    test(dl, "1980-08-23T00:00+0000", "1980-08-24T00:00+0000")
-    test(dl, "2024-08-23T00:00+0000")
+    test(dl, "2008-11-09T14:00+0000", "2008-11-09T14:30+0000")
+
+
+def test_gridsat_goes() -> None:
+    pd = ProductLocatorGC("F", "G12")
+    ds = DatasourceHTTP(pd)
+    dl = Downloader(datasource=ds, locator=pd, date_format=DATE_FORMAT)
+
+    test(dl, "2008-11-09T14:00+0000")
 
 
 def test_goes() -> None:
@@ -56,8 +57,9 @@ def test_goes() -> None:
 
 
 def main() -> None:
-    test_gridsat_aws()
+    test_gridsat_goes()
     test_gridsat_http()
+    test_gridsat_aws()
     test_goes()
 
 
