9b24f70f7276f12bc70ba550a8a3ee727f27404a
wvenialbo <wvenialbo@gmail.com>
2024-10-19 20:37:23 -0300

    docs(goes-dl): add docstring for 'GOES_DL' module

--- src/GOES_DL/__init__.py
+++ src/GOES_DL/__init__.py
@@ -1,3 +1,7 @@
+"""
+Provide functionalities for downloading GOES and GridSat datasets.
+"""
+
 __package_id__ = "GOES-DL"
 __package_name__ = f"GOES Dataset Downloader - {__package_id__}"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
