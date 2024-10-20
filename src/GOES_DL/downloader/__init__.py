5c8291ffe74841be1e9a3cc7f517e7a461cfe4dc
wvenialbo <wvenialbo@gmail.com>
2024-10-20 10:01:14 -0300

    docs(downloader): add module docstring
    Add a module docstring to clarify the purpose of the downloader module.
    
    Co-authored-by: [Author Name]

--- src/GOES_DL/downloader/__init__.py
+++ src/GOES_DL/downloader/__init__.py
@@ -1,1 +1,13 @@
+"""
+Export the class for the downloader object.
+
+The downloader object is responsible for downloading files from a
+specified location.
+
+Classes
+-------
+Downloader
+    Abstract a downloader object.
+"""
+
 from .downloader import Downloader as Downloader
