45878a350234b024892a1d75abd5b0762dcff504
wvenialbo <wvenialbo@gmail.com>
2024-10-20 04:07:08 -0300

    refactor(goes-dl): set the pre-release version
    Set the pre-release version to "v0.1-rc1".

--- src/GOES_DL/__init__.py
+++ src/GOES_DL/__init__.py
@@ -4,4 +4,4 @@ Provide functionalities for downloading GOES and GridSat datasets.
 
 __package_id__ = "GOES-DL"
 __package_name__ = f"GOES Dataset Downloader - {__package_id__}"
-__version__ = "0.1.0"
+__version__ = "v0.1-rc1"
