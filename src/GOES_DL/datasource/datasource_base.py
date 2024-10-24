7a0d05aca8e7283ae530f78c983f15447065249c
wvenialbo <wvenialbo@gmail.com>
2024-10-24 19:29:06 -0300

    feat(datasource): add DatasourceBase class
    Add DatasourceBase class with cache and repository support.

---
+++ src/GOES_DL/datasource/datasource_base.py
@@ -0,0 +1,56 @@
+"""
+Extend the Datasource interface with cache and repository support.
+
+Classes:
+    DatasourceBase: Extend the Datasource interface.
+"""
+
+from .datasource import Datasource
+from .datasource_cache import DatasourceCache
+from .datasource_repository import DatasourceRepository
+
+
+class DatasourceBase(Datasource):
+    """
+    Extend the Datasource interface with cache and repository support.
+
+    Attributes
+    ----------
+    cache : DatasourceCache
+        The cache for the datasource.
+    repository : DatasourceRepository
+        The repository for the datasource.
+    """
+
+    cache: DatasourceCache
+    repository: DatasourceRepository
+
+    def __init__(
+        self,
+        base_url: str,
+        repository: str | DatasourceRepository,
+        cache: float | DatasourceCache,
+    ) -> None:
+        """
+        Initialize the DatasourceBase.
+
+        Parameters
+        ----------
+        base_url : str
+            The base URL for the datasource.
+        repository : str | DatasourceRepository
+            The repository for the datasource. If a string is provided,
+            it will be used as the base path for the repository.
+        cache : float | DatasourceCache
+            The cache for the datasource. If a float is provided, it
+            will be used as the life time for each entry in the cache.
+        """
+        super().__init__(base_url)
+        if isinstance(repository, str):
+            base_path: str = repository
+            repository = DatasourceRepository(base_path)
+        self.repository = repository
+        if isinstance(cache, float):
+            life_time: float = cache
+            cache = DatasourceCache(life_time)
+        self.cache = cache
