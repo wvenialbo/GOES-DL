7138c7c763f7582b69b0788541ec34a6bc09486d
wvenialbo <wvenialbo@gmail.com>
2024-10-24 11:24:36 -0300

    refactor(downloader): remove DownloaderRepository class

--- src/GOES_DL/downloader/__init__.py
+++ src/GOES_DL/downloader/__init__.py
@@ -8,9 +8,6 @@ Classes
 -------
 Downloader
     Abstract a downloader object.
-DownloaderRepository
-    Manage file operations for the downloader object.
 """
 
 from .downloader import Downloader as Downloader
-from .downloader_repository import DownloaderRepository as DownloaderRepository
