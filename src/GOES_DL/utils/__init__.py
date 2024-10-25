4eafb4592c20eca99a6a81af38c9974596f71b51
wvenialbo <wvenialbo@gmail.com>
2024-10-25 02:02:43 -0300

    refactor(utils): url -> URL utility class name

--- src/GOES_DL/utils/__init__.py
+++ src/GOES_DL/utils/__init__.py
@@ -9,4 +9,4 @@ Classes:
 
 from .file_repository import FileRepository as FileRepository
 from .headers import RequestHeaders as RequestHeaders
-from .url import url as url
+from .url import URL as URL
