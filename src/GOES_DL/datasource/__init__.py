aabe4d75bd77b0100a8a098a986937dedd543be1
wvenialbo <wvenialbo@gmail.com>
2024-10-25 02:24:59 -0300

    refactor(datasource): add __all__ to __init__.py

--- src/GOES_DL/datasource/__init__.py
+++ src/GOES_DL/datasource/__init__.py
@@ -24,3 +24,11 @@ from .datasource_aws import DatasourceAWS as DatasourceAWS
 from .datasource_cache import DatasourceCache as DatasourceCache
 from .datasource_http import DatasourceHTTP as DatasourceHTTP
 from .datasource_repository import DatasourceRepository as DatasourceRepository
+
+__all__ = [
+    "Datasource",
+    "DatasourceAWS",
+    "DatasourceCache",
+    "DatasourceHTTP",
+    "DatasourceRepository",
+]
