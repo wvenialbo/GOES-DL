9af40fb28a07eb0afe2d7dd95ca00bf34d64e64e
wvenialbo <wvenialbo@gmail.com>
2024-10-29 10:20:52 -0300

    feat(datasource): add method to download a file
    Add method to just download a file and save it to a local repository.
    The method do not load the file into memory, it just download it and
    save it.

--- src/GOES_DL/datasource/datasource.py
+++ src/GOES_DL/datasource/datasource.py
@@ -15,8 +15,8 @@ class Datasource(ABC):
     Abstract a datasource object.
 
     This class defines the interface for a datasource object. The
-    datasource is responsible for listing the contents of a directory
-    in a remote location and for downloading files from that location.
+    datasource is responsible for listing the contents of a directory in
+    a remote location and for downloading files from that location.
 
     Attributes
     ----------
@@ -27,8 +27,11 @@ class Datasource(ABC):
 
     Methods
     -------
+    download_file(file_path: str)
+        Retrieve a file from the datasource and save it into the local
+        repository.
     get_file(file_path: str)
-        Get a file from the datasource.
+        Get a file from the datasource or local repository.
     listdir(dir_path: str)
         List the contents of a directory.
     """
@@ -36,12 +39,31 @@ class Datasource(ABC):
     base_url: str
 
     @abstractmethod
+    def download_file(self, file_path: str) -> bytes:
+        """
+        Download a file from the datasource into the local repository.
+
+        Get a file from a remote location or local repository. The path
+        is relative to the base URL and local repository root directory.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the file. The path is relative to the base URL.
+
+        Returns
+        -------
+        bytes
+            The file object.
+        """
+
+    @abstractmethod
     def get_file(self, file_path: str) -> bytes:
         """
-        Get a file.
+        Get a file from the datasource or local repository.
 
-        Get a file from a remote location. The path is relative to the
-        base URL.
+        Get a file from a remote location or local repository. The path
+        is relative to the base URL and local repository root directory.
 
         Parameters
         ----------
@@ -57,7 +79,7 @@ class Datasource(ABC):
     @abstractmethod
     def listdir(self, dir_path: str) -> list[str]:
         """
-        List the contents of a directory.
+        List the contents of a remote directory.
 
         List the contents of a directory in a remote location. The path
         is relative to the base URL.
