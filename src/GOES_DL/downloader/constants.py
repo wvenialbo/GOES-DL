a739b418b6ac8ceb6e56c41495422d2a31d03b53
wvenialbo <wvenialbo@gmail.com>
2025-01-10 08:40:48 -0300

    docs(downloader): update constants documentation
    Update constants documentation to clarify module scope and add new
    attributes to clarify module scope and add new attributes.

--- src/GOES_DL/downloader/constants.py
+++ src/GOES_DL/downloader/constants.py
@@ -1,13 +1,19 @@
 """
-Define and export constants used across the GOES-DL project.
+Define and export constants used across the GOES-DL downloader module.
 
-These constants include date and time formats, file naming patterns, and other
-fixed values that are used throughout the project.
+These constants include date and time formats, and other fixed values
+that are used throughout the module.
 
 Attributes
 ----------
 ISO_TIMESTAMP_FORMAT : str
     The format string for ISO 8601 timestamps.
+TIME_TOLERANCE_DEFAULT : int
+    The default value for the time tolerance.
+TIME_TOLERANCE_MAX : int
+    The upper limit for the time tolerance.
+TIME_TOLERANCE_MIN : int
+    The lower limit for the time tolerance.
 """
 
 ISO_TIMESTAMP_FORMAT: str = "%Y-%m-%dT%H:%M:%S%z"
