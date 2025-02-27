abba3a07e4dbecd59ddcf33b028d8713964f604f
wvenialbo <wvenialbo@gmail.com>
2025-02-26 21:10:59 -0300

    docs(__init__): standardize package name
    Standardize package name for consistency.

--- src/goesdl/__init__.py
+++ src/goesdl/__init__.py
@@ -1,5 +1,7 @@
 """Provide functionalities for downloading GOES and GridSat datasets."""
 
 __package_id__ = "GOES-DL"
-__package_name__ = f"GOES Dataset Downloader - {__package_id__}"
+__package_name__ = (
+    f"{__package_id__} — GOES Satellite Imagery Dataset Downloader"
+)
 __version__ = "v0.1-rc4"
