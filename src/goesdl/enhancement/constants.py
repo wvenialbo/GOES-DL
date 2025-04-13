15c30948371043fd364d86c8661cc23ae8e92365
wvenialbo <wvenialbo@gmail.com>
2025-04-13 05:15:06 -0300

    feat(enhancement): add constant and typings

---
+++ src/goesdl/enhancement/constants.py
@@ -0,0 +1,15 @@
+"""Provide constants for the enhancement utility module."""
+
+BRG_MAX = 255.0
+CLR_MAX = 255.0
+CMYK_MAX = 100.0
+HUE_MAX = 360.0
+HSV_MAX = 100.0
+
+CM_BGR = "BGR"
+CM_RGB = "RGB"
+CM_CMYK = "CMYK"
+CM_GRAY = "GRAY"
+CM_HSV = "HSV"
+
+UNNAMED_TABLE = "UNNAMED"
---
+++ src/goesdl/enhancement/shared.py
@@ -0,0 +1,17 @@
+"""
+Provide methods for handling IR brightness temperature color enhancement.
+
+It includes functions to load, parse, process, and save McIDAS and GMT
+enhancement color tables, as well as reverse and manipulate color maps.
+"""
+
+CMYKValue = tuple[float, float, float, float]
+ColorEntry = tuple[float, float, float, float]
+DomainData = tuple[float, float]
+RGBValue = tuple[float, float, float]
+ValueTables = tuple[list[float], ...]
+
+ColorTable = list[RGBValue]
+PaletteTable = list[ColorEntry]
+PaletteItem = tuple[PaletteTable, ColorTable]
+StretchingTable = list[tuple[float, float]]
