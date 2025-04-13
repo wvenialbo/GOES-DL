0d2e97769bf3a9aeefa9bac78df1a4edc3ea5935
wvenialbo <wvenialbo@gmail.com>
2025-04-13 08:43:22 -0300

    refactor(enhancement): simplify shared typing

--- src/goesdl/enhancement/shared.py
+++ src/goesdl/enhancement/shared.py
@@ -5,13 +5,14 @@ It includes functions to load, parse, process, and save McIDAS and GMT
 enhancement color tables, as well as reverse and manipulate color maps.
 """
 
-CMYKValue = tuple[float, float, float, float]
 ColorEntry = tuple[float, float, float, float]
 DomainData = tuple[float, float]
+
+CMYKValue = ColorEntry
 RGBValue = tuple[float, float, float]
-ValueTables = tuple[list[float], ...]
 
 ColorTable = list[RGBValue]
 PaletteData = list[ColorEntry]
 PaletteItem = tuple[PaletteData, ColorTable]
 StretchingTable = list[tuple[float, float]]
+ValueTables = tuple[list[float], ...]
