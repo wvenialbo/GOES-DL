f28eed9e0eef5e436be02a54fe6c56768798a010
wvenialbo <wvenialbo@gmail.com>
2025-04-17 03:03:25 -0300

    refactor(enhancement): document the preview function

--- src/goesdl/enhancement/preview.py
+++ src/goesdl/enhancement/preview.py
@@ -19,17 +19,24 @@ def show_colormap(
     # Color scale map preview plot
     fig, ax = plt.subplots()
 
+    # Plot the example data
     tmin, tmax = scale.extent
     im = ax.imshow(data, aspect="auto", cmap=scale.cmap, vmin=tmin, vmax=tmax)
 
+    # Create the color bar with the measurement scale
     cbar = fig.colorbar(im, orientation="horizontal")
     cbar_ref = ColorbarTicks(scale.domain, nticks, 0)
     cbar.set_ticks(cbar_ref.cticks)
 
+    # Note: Alternatively `norm=scale.cnorm` can be passed to imshow and
+    # `cbar.ax.minorticks_off()` is used to inhibit the minor ticks.
+
+    # Hide the y-axis since it does not make sense
     ax.tick_params(
         axis="y", which="both", left=False, right=False, labelleft=False
     )
 
+    # Set the title, x-axis label and the colorbar caption
     plt.title(scale.name)
 
     ax.set_xlabel("Color Index", color="black", labelpad=3.0)
@@ -38,8 +45,5 @@ def show_colormap(
         measurement = "Measurement"
 
     cbar.set_label(label=measurement, color="black", weight="normal")
-
-    # Note: Alternatively `norm=scale.cnorm` can be passed to imshow and
-    # `cbar.ax.minorticks_off()` is used to inhibit the minor ticks.
 
     plt.show()
