93c5b67364fe5fb9c06c6271e4268eb8414238ba
wvenialbo <wvenialbo@gmail.com>
2025-04-17 02:46:36 -0300

    refactor(enhancement): improve colormap preview

--- src/goesdl/enhancement/preview.py
+++ src/goesdl/enhancement/preview.py
@@ -6,7 +6,10 @@ from .ticks import ColorbarTicks
 
 
 def show_colormap(
-    scale: EnhancementScale, offset: float = 0.0, nticks: int = 14
+    scale: EnhancementScale,
+    offset: float = 0.0,
+    nticks: int = 14,
+    measurement: str = "",
 ) -> None:
     # Example data
     vmin, vmax = scale.domain
@@ -16,14 +19,25 @@ def show_colormap(
     # Color scale map preview plot
     fig, ax = plt.subplots()
 
-    plt.title(scale.name)
-
     tmin, tmax = scale.extent
     im = ax.imshow(data, aspect="auto", cmap=scale.cmap, vmin=tmin, vmax=tmax)
 
     cbar = fig.colorbar(im, orientation="horizontal")
     cbar_ref = ColorbarTicks(scale.domain, nticks, 0)
     cbar.set_ticks(cbar_ref.cticks)
+
+    ax.tick_params(
+        axis="y", which="both", left=False, right=False, labelleft=False
+    )
+
+    plt.title(scale.name)
+
+    ax.set_xlabel("Color Index", color="black", labelpad=3.0)
+
+    if not measurement:
+        measurement = "Measurement"
+
+    cbar.set_label(label=measurement, color="black", weight="normal")
 
     # Note: Alternatively `norm=scale.cnorm` can be passed to imshow and
     # `cbar.ax.minorticks_off()` is used to inhibit the minor ticks.
