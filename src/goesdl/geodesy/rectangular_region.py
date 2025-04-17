eb6943dee7d70b5e67f26e81265b8d998ef8aebb
wvenialbo <wvenialbo@gmail.com>
2025-04-17 03:53:28 -0300

    feat(geodesy): add tick attributes to RectangularRegion class

--- src/goesdl/geodesy/rectangular_region.py
+++ src/goesdl/geodesy/rectangular_region.py
@@ -1,19 +1,39 @@
 from collections.abc import Generator
-from math import degrees
+from math import degrees, floor
 from typing import Literal, cast
+
+from numpy import arange, float32
+
+from ..utils.array import ArrayFloat32
 
 CoordDomain = tuple[float, float, float, float]
 CoordLimits = tuple[float, float]
 ExtentType = tuple[float, float]
 CenterType = tuple[float, float]
+StepType = tuple[float, float]
+
+DEFAULT_STEP = (2.0, 2.0)
 
 
 class RectangularRegion:
 
     domain: CoordDomain
 
+    xticks: ArrayFloat32
+    yticks: ArrayFloat32
+
     def __init__(self, domain: CoordDomain) -> None:
         self.domain = domain
+
+        self.xticks, self.yticks = self._create_grid_ticks(
+            domain, DEFAULT_STEP
+        )
+
+    def set_ticks(
+        self, domain: CoordDomain | None = None, step: StepType = DEFAULT_STEP
+    ) -> None:
+        domain = self.domain if domain is None else domain
+        self.xticks, self.yticks = self._create_grid_ticks(domain, step)
 
     @classmethod
     def from_central_point(
@@ -44,6 +64,21 @@ class RectangularRegion:
         domain = tuple(lons_deg + lats_deg)
 
         return cls(cast(CoordDomain, domain))
+
+    @staticmethod
+    def _create_grid_ticks(
+        domain: CoordDomain, step: StepType
+    ) -> tuple[ArrayFloat32, ArrayFloat32]:
+        dx, dy = step
+        lon_min = dx * floor(domain[0] / dx)
+        lon_max = dx * floor(domain[1] / dx) + dx
+        xticks = arange(lon_min, lon_max, dx).astype(float32)
+
+        lat_min = dy * floor(domain[2] / dy)
+        lat_max = dy * floor(domain[3] / dy) + dy
+        yticks = arange(lat_min, lat_max, dy).astype(float32)
+
+        return cast(ArrayFloat32, xticks), cast(ArrayFloat32, yticks)
 
     @property
     def extent(self) -> CoordDomain:
