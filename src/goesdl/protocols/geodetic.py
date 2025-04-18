5c458f88b3dcd85dabd67365c0b347f8f693677c
wvenialbo <wvenialbo@gmail.com>
2025-04-18 11:03:50 -0300

    docs(protocols): add GeodeticRegion protocol documentation

--- src/goesdl/protocols/geodetic.py
+++ src/goesdl/protocols/geodetic.py
@@ -9,14 +9,71 @@ RegionExtent = tuple[float, float, float, float]
 
 
 class GeodeticRegion(Protocol):
+    """
+    A protocol for geodetic region of interest (ROI).
+
+    The region of interest is defined by a pair of coordinate ranges
+    (longitude and latitude) and a projection.  The region is used to
+    slice the data.  The projection is used to transform the data.
+
+    Attributes
+    ----------
+    domain : RegionDomain
+        The domain of the region of interest as a pair of coordinate
+        ranges.  The first coordinate range is the longitude range
+        (min_lon, max_lon) and the second coordinate range is the
+        latitude range (min_lat, max_lat).
+    xticks : ArrayFloat32
+        The x-ticks of the region of interest. Helper for plotting.
+        The x-ticks are the longitude values of the region of interest.
+    yticks : ArrayFloat32
+        The y-ticks of the region of interest. Helper for plotting.
+        The y-ticks are the latitude values of the region of interest.
+    extent : RegionExtent
+        The extent of the region of interest as a tuple of (min_lon,
+        max_lon, min_lat, max_lat).
+    lat_bounds : CoordRange
+        The latitude bounds of the region of interest as a tuple of
+        (min_lat, max_lat).
+    lon_bounds : CoordRange
+        The longitude bounds of the region of interest as a tuple of
+        (min_lon, max_lon).
+    """
 
     domain: RegionDomain
-
-    xticks: ArrayFloat32
-    yticks: ArrayFloat32
+    """
+    The domain of the region of interest as a pair of coordinate
+    ranges.  The first coordinate range is the longitude range
+    (min_lon, max_lon) and the second coordinate range is the
+    latitude range (min_lat, max_lat).
+    """
 
     extent: RegionExtent
+    """
+    The extent of the region of interest as a tuple of (min_lon,
+    max_lon, min_lat, max_lat).
+    """
 
     lat_bounds: CoordRange
+    """
+    The latitude bounds of the region of interest as a tuple of
+    (min_lat, max_lat).
+    """
 
     lon_bounds: CoordRange
+    """
+    The longitude bounds of the region of interest as a tuple of
+    (min_lon, max_lon).
+    """
+
+    xticks: ArrayFloat32
+    """
+    The x-ticks of the region of interest. Helper for plotting.
+    The x-ticks are the longitude values of the region of interest.
+    """
+
+    yticks: ArrayFloat32
+    """
+    The y-ticks of the region of interest. Helper for plotting.
+    The y-ticks are the latitude values of the region of interest.
+    """
