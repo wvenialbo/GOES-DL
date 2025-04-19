e11b4050192eae7ff7af22d8278f52198fd8fcb4
wvenialbo <wvenialbo@gmail.com>
2025-04-19 10:49:01 -0300

    refactor(geosproj_parameters): convert GeosProjParameters class to Protocol for improved type hinting

--- src/goesdl/geodesy/geosproj_parameters.py
+++ src/goesdl/geodesy/geosproj_parameters.py
@@ -1,7 +1,9 @@
+from typing import Protocol
+
 from numpy import float64
 
 
-class GeosProjParameters:
+class GeosProjParameters(Protocol):
     """
     A class to store the geostationary projection parameters.
 
@@ -19,25 +21,3 @@ class GeosProjParameters:
     longitude_of_projection_origin: float64
     perspective_point_height: float64
     sweep_angle_axis: str
-
-    def __init__(
-        self,
-        longitude_of_projection_origin: float64,
-        perspective_point_height: float64,
-        sweep_angle_axis: str,
-    ) -> None:
-        """
-        Initialize the GeostationaryParameters object.
-
-        Parameters
-        ----------
-        longitude_of_projection_origin : float64
-            The longitude of the projection origin in East degrees.
-        perspective_point_height : float64
-            The height of the perspective point in meters.
-        sweep_angle_axis : str
-            The axis of the sweep angle.
-        """
-        self.longitude_of_projection_origin = longitude_of_projection_origin
-        self.perspective_point_height = perspective_point_height
-        self.sweep_angle_axis = sweep_angle_axis
