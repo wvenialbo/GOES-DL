78511bbb34091db078dd53989dafb946203456d8
wvenialbo <wvenialbo@gmail.com>
2025-04-19 10:47:38 -0300

    refactor(globe_parameters): convert GlobeParameters class to Protocol for better type hinting

--- src/goesdl/geodesy/globe_parameters.py
+++ src/goesdl/geodesy/globe_parameters.py
@@ -1,7 +1,9 @@
+from typing import Protocol
+
 from numpy import float64
 
 
-class GlobeParameters:
+class GlobeParameters(Protocol):
     """
     A class to store the globe parameters.
 
@@ -18,37 +20,3 @@ class GlobeParameters:
     semi_major_axis: float64
     semi_minor_axis: float64
     inverse_flattening: float64
-
-    def __init__(
-        self,
-        semi_major_axis: float64,
-        semi_minor_axis: float64,
-        inverse_flattening: float64,
-    ) -> None:
-        """
-        Initialize the GlobeParameters object.
-
-        Parameters
-        ----------
-        semi_major_axis : float64
-            The semi-major axis of the ellipsoid in meters.
-        semi_minor_axis : float64
-            The semi-minor axis of the ellipsoid in meters.
-        inverse_flattening : float64
-            The inverse flattening of the ellipsoid.
-        """
-        self.semi_major_axis = semi_major_axis
-        self.semi_minor_axis = semi_minor_axis
-        self.inverse_flattening = inverse_flattening
-
-    @property
-    def flattening(self) -> float64:
-        """
-        Get the flattening of the globe.
-
-        Returns
-        -------
-        np.float64
-            The flattening of the globe.
-        """
-        return 1.0 / self.inverse_flattening
