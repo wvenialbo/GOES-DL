dcb03a99eda7798ae4d8474165dd298812f8ccaa
wvenialbo <wvenialbo@gmail.com>
2025-04-20 14:13:00 -0300

    feat(trackers): initialize track_parsers_stock with HURDAT2 parser

--- src/goesdl/trackers/tracker_stock.py
+++ src/goesdl/trackers/tracker_stock.py
@@ -1,8 +1,11 @@
+from .hurdat2 import TrackParserHurdat2
 from .track_parser import TrackParser
 
-track_parsers_stock: dict[str, type[TrackParser]] = {}
+track_parsers_stock: dict[str, type[TrackParser]] = {
+    TrackParserHurdat2.ID: TrackParserHurdat2,
+}
 
-DEFAULT_TRACKER_ID = ""
+DEFAULT_TRACKER_ID = TrackParserHurdat2.ID
 
 
 def validate_supported_dataset(dataset: str) -> str:
