9f2c2211f11ac519a4b540b5f059becf12654590
wvenialbo <wvenialbo@gmail.com>
2025-04-21 01:07:27 -0300

    refactor(goesr): simplify GOESCoverageTime initialization by using attributes for datetime fields

--- src/goesdl/goesr/netcdf_time.py
+++ src/goesdl/goesr/netcdf_time.py
@@ -1,26 +1,16 @@
 from datetime import datetime
-from typing import cast
 
-from netCDF4 import Dataset  # pylint: disable=no-name-in-module
-from numpy import datetime64
-
-from ..netcdf import HasStrHelp
-
-NOT_A_DATETIME = cast(datetime, datetime64("NaT"))
+from ..netcdf import DatasetView, attribute
 
 
-class GOESCoverageTime(HasStrHelp):
+class GOESCoverageTime(DatasetView):
 
-    datetime_start: datetime = NOT_A_DATETIME
-    datetime_end: datetime = NOT_A_DATETIME
-
-    def __init__(self, record: Dataset) -> None:
-        datetime_start = getattr(record, "time_coverage_start", "")
-        datetime_end = getattr(record, "time_coverage_end", "")
-
-        if datetime_start and datetime_end:
-            self.datetime_start = datetime.fromisoformat(datetime_start)
-            self.datetime_end = datetime.fromisoformat(datetime_end)
+    datetime_start: datetime = attribute(
+        "time_coverage_start", convert=datetime.fromisoformat
+    )
+    datetime_end: datetime = attribute(
+        "time_coverage_end", convert=datetime.fromisoformat
+    )
 
     @property
     def timestamp_start(self) -> float:
