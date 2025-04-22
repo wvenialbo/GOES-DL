e4f3a024fa099e848e97c426c0649d2335a66f10
wvenialbo <wvenialbo@gmail.com>
2025-04-22 18:33:43 -0300

    refactor(goesr): move datetime_midpoint calculation to a separate property

--- src/goesdl/goesr/netcdf_time.py
+++ src/goesdl/goesr/netcdf_time.py
@@ -13,9 +13,18 @@ class GOESCoverageTime(DatasetView):
     )
 
     @property
-    def timestamp_start(self) -> float:
-        return self.datetime_start.timestamp()
+    def datetime_midpoint(self) -> datetime:
+        delta = self.datetime_end - self.datetime_start
+        return self.datetime_start + delta / 2
 
     @property
     def timestamp_end(self) -> float:
         return self.datetime_end.timestamp()
+
+    @property
+    def timestamp_midpoint(self) -> float:
+        return self.datetime_midpoint.timestamp()
+
+    @property
+    def timestamp_start(self) -> float:
+        return self.datetime_start.timestamp()
