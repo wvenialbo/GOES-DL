381c2a43e5f534c6c87df16616d2167e441ff177
wvenialbo <wvenialbo@gmail.com>
2025-04-22 16:36:37 -0300

    fix(array): add uint16 and uint32 array types to array.py

--- src/goesdl/utils/array.py
+++ src/goesdl/utils/array.py
@@ -1,10 +1,23 @@
-from numpy import bool_, dtype, float32, float64, int8, int16, int32, ndarray
+from numpy import (
+    bool_,
+    dtype,
+    float32,
+    float64,
+    int8,
+    int16,
+    int32,
+    ndarray,
+    uint16,
+    uint32,
+)
 from numpy.ma import MaskedArray
 
 ArrayBool = ndarray[tuple[int, ...], dtype[bool_]]
 ArrayInt8 = ndarray[tuple[int, ...], dtype[int8]]
 ArrayInt16 = ndarray[tuple[int, ...], dtype[int16]]
 ArrayInt32 = ndarray[tuple[int, ...], dtype[int32]]
+ArrayUint16 = ndarray[tuple[int, ...], dtype[uint16]]
+ArrayUint32 = ndarray[tuple[int, ...], dtype[uint32]]
 ArrayFloat32 = ndarray[tuple[int, ...], dtype[float32]]
 ArrayFloat64 = ndarray[tuple[int, ...], dtype[float64]]
 
