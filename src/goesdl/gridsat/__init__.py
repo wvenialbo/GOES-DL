73637f05198106600bd64a8ce993a8a23b3372f2
wvenialbo <wvenialbo@gmail.com>
2025-04-23 10:56:14 -0300

    refactor(gridsat): remove netcdf_info module

--- src/goesdl/gridsat/__init__.py
+++ src/goesdl/gridsat/__init__.py
@@ -7,15 +7,10 @@ Classes:
 """
 
 from .dataset_info import GSDatasetInfo
-from .locator_b1 import GridSatProductLocatorB1
-from .locator_gc import GridSatProductLocatorGC
 from .geodetic import GSLatLonGrid
 from .image import GSImage
-from .netcdf_info import (
-    GSGeospatialInfo,
-    GSPlatformInfo,
-    GSRadiometricInfo,
-)
+from .locator_b1 import GridSatProductLocatorB1
+from .locator_gc import GridSatProductLocatorGC
 from .projection import GSGlobe, GSOrbitGeometry
 from .time import GSCoverageTime, GSTimeGrid
 from .utility import read_gridsat_dataset
@@ -28,9 +23,6 @@ __all__ = [
     "GSTimeGrid",
     "GSCoverageTime",
     "GSDatasetInfo",
-    "GSPlatformInfo",
-    "GSGeospatialInfo",
-    "GSRadiometricInfo",
     "read_gridsat_dataset",
     "GSOrbitGeometry",
     "GSGlobe",
