772379781c64da1347454c728e8f0d838018884c
wvenialbo <wvenialbo@gmail.com>
2025-04-25 13:03:25 -0300

    refactor(enhancement): remove compress_color_data function to streamline utility module

--- src/goesdl/enhancement/utility.py
+++ src/goesdl/enhancement/utility.py
@@ -2,34 +2,6 @@
 
 from collections.abc import Sequence
 
-from .shared import ColorSegment
-
-
-def compress_color_data(color_data: list[ColorSegment]) -> list[ColorSegment]:
-    """
-    Compress color data by removing duplicate entries.
-
-    Parameters
-    ----------
-    color_data : list of tuple of float
-        List of color segments.
-
-    Returns
-    -------
-    list of tuple of float
-        Compressed color table.
-    """
-    compressed: list[ColorSegment] = [color_data[0]]
-
-    for x2, c2, d2 in color_data[1:]:
-        x1, c1, _ = compressed[-1]
-        if x1 == x2:
-            compressed[-1] = (x1, c1, d2)
-        else:
-            compressed.append((x2, c2, d2))
-
-    return compressed
-
 
 def _interpv(x: float, x0: float, x1: float, y0: float, y1: float) -> float:
     slope = (y1 - y0) / (x1 - x0)
