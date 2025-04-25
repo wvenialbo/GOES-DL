a3986d6fcaf4e11d7d30f01f134a6dbadb45126e
wvenialbo <wvenialbo@gmail.com>
2025-04-25 13:08:20 -0300

    fix(enhancement): add missing type annotation for segment_data in SegmentedColormap class

--- src/goesdl/enhancement/colormap.py
+++ src/goesdl/enhancement/colormap.py
@@ -22,6 +22,8 @@ from .shared import (
 
 class SegmentedColormap:
 
+    segment_data: SegmentData
+
     def __init__(self, raw_segment_data: GSegmentData) -> None:
         segment_data = self._copy_segment_data(raw_segment_data)
 
