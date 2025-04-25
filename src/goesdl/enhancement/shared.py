457ed6e47e763ec227017aaa5ea39f36285a4d56
wvenialbo <wvenialbo@gmail.com>
2025-04-25 09:32:39 -0300

    feat(enhancement): add type annotations for color handling and segment data

--- src/goesdl/enhancement/shared.py
+++ src/goesdl/enhancement/shared.py
@@ -5,6 +5,9 @@ It includes functions to load, parse, process, and save McIDAS and GMT
 enhancement color tables, as well as reverse and manipulate color maps.
 """
 
+from collections.abc import Sequence
+from typing import Any
+
 ColorEntry = tuple[float, float, float, float]
 ColorSegment = tuple[float, ...]
 DomainData = tuple[float, float]
@@ -17,3 +20,18 @@ PaletteData = list[ColorEntry]
 PaletteItem = tuple[PaletteData, ColorTable]
 StretchingTable = list[tuple[float, float]]
 ValueTables = tuple[list[float], ...]
+
+# Listed colours
+
+GColorValue = Sequence[Any]
+GListedColors = Sequence[GColorValue]
+
+ListedColors = list[RGBValue]
+
+# Colour segments
+
+GSegmentEntry = Sequence[Any]
+GSegmentData = dict[str, Sequence[GSegmentEntry]]
+
+SegmentEntry = tuple[float, float, float]
+SegmentData = dict[str, list[SegmentEntry]]
