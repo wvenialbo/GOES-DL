aabd9d8bd67199a31ca883b268d253147ee7f00f
wvenialbo <wvenialbo@gmail.com>
2025-04-27 12:33:11 -0300

    refactor(enhancement): organize and group type annotations for color data structures

--- src/goesdl/enhancement/shared.py
+++ src/goesdl/enhancement/shared.py
@@ -22,19 +22,24 @@ ValueTables = tuple[list[float], ...]
 
 # Listed colours
 
+# Generic discrete colour map list
 GColorValue = Sequence[Any]
 GListedColors = Sequence[GColorValue]
 
+# Natively defined discrete colour map list
 ListedColors = list[RGBValue]
 
 # Colour segments
 
+# Generic colour segment data
 GSegmentEntry = Sequence[Any]
 GSegmentData = dict[str, Sequence[GSegmentEntry]]
 
+# Colour segment data as defined in Matplotlib
 MColorSegment = tuple[float, ...]
 MComponents = Literal["red", "green", "blue", "alpha"]
 MSegmentData = dict[MComponents, Sequence[MColorSegment]]
 
+# Natively defined colour segment data
 ColorSegment = tuple[float, float, float]
 SegmentData = dict[str, list[ColorSegment]]
