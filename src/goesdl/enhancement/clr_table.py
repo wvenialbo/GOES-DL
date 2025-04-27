955e8192e2def04457e9fc5b4ce69ea33c1b6357
wvenialbo <wvenialbo@gmail.com>
2025-04-27 17:04:17 -0300

    refactor(enhancement): update type hints for color normalization methods to use ColorValueList and KeypointList

--- src/goesdl/enhancement/clr_table.py
+++ src/goesdl/enhancement/clr_table.py
@@ -1,5 +1,5 @@
 from .constants import BRG_MAX, CLR_MAX
-from .shared import ColorTable, ValueTable
+from .shared import ColorTable, ColorValueList, KeypointList, ValueTable
 
 
 class clr_utility:
@@ -10,15 +10,15 @@ class clr_utility:
         return list(zip(x, b, g, r))
 
     @staticmethod
-    def _normalize_colors(y: list[float]) -> list[float]:
+    def _normalize_colors(y: ColorValueList) -> ColorValueList:
         return [k / CLR_MAX for k in y]
 
     @staticmethod
-    def _normalize_grayscale(y: list[float]) -> list[float]:
+    def _normalize_grayscale(y: ColorValueList) -> ColorValueList:
         return [k / BRG_MAX for k in y]
 
     @staticmethod
-    def _normalize_values(x: list[float]) -> list[float]:
+    def _normalize_values(x: KeypointList) -> KeypointList:
         x_min = x[0]
         length = x[-1] - x_min
         return [(k - x_min) / length for k in x]
