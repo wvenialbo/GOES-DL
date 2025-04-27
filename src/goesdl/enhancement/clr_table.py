2e502cfd7915e47f92c8175ebd5efc046c047a3e
wvenialbo <wvenialbo@gmail.com>
2025-04-27 05:59:26 -0300

    refactor(clr_utility): rename parameter in _make_color_entries for clarity

--- src/goesdl/enhancement/clr_table.py
+++ src/goesdl/enhancement/clr_table.py
@@ -6,11 +6,8 @@ class clr_utility:
 
     @classmethod
     def _make_color_entries(
-        cls, j: list[float], b: list[float], g: list[float], r: list[float]
+        cls, x: list[float], b: list[float], g: list[float], r: list[float]
     ) -> list[ColorEntry]:
-        length = j[-1] - j[0]
-        x = [(k - j[0]) / length for k in j]
-
         return list(zip(x, b, g, r))
 
     @staticmethod
