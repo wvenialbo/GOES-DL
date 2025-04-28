5fc0cff8fa33e9a7dcdadbcccb587e4f69ff7125
wvenialbo <wvenialbo@gmail.com>
2025-04-28 20:31:02 -0300

    refactor(enhancement): simplify show_colormap by removing offset adjustments and unused ColorbarTicks

--- src/goesdl/enhancement/preview.py
+++ src/goesdl/enhancement/preview.py
@@ -2,33 +2,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from .scale import EnhancementScale
-from .ticks import ColorbarTicks
 
 
 def show_colormap(
-    scale: EnhancementScale,
-    measurement: str = "",
-    offset: float = 0.0,
-    nticks: int = 14,
+    scale: EnhancementScale, measurement: str = "", offset: float = 0.0
 ) -> None:
     # Example data
     vmin, vmax = scale.domain
-    vmin, vmax = vmin + offset, vmax + offset
     data = np.linspace(vmin, vmax, scale.ncolors)[None, :]
 
     # Color scale map preview plot
     fig, ax = plt.subplots()
 
     # Plot the example data
-    tmin, tmax = scale.extent
-    tmin, tmax = tmin + offset, tmax + offset
-    im = ax.imshow(data, aspect="auto", cmap=scale.cmap, vmin=tmin, vmax=tmax)
+    im = ax.imshow(data, aspect="auto", cmap=scale.cmap, norm=scale.cnorm)
 
     # Create the color bar with the measurement scale
     cbar = fig.colorbar(im, orientation="horizontal")
 
-    cbar_ref = ColorbarTicks((tmin, tmax), nticks, 0)
-    cbar.set_ticks(cbar_ref.cticks)
+    cbar.set_ticks(scale.cticks)
+    ticklabels = scale.get_ticklabels(offset, int)
+    cbar.set_ticklabels(ticklabels)
 
     # Note: Alternatively `norm=scale.cnorm` can be passed to imshow and
     # `cbar.ax.minorticks_off()` is used to inhibit the minor ticks.
@@ -39,7 +33,7 @@ def show_colormap(
     )
 
     # Set the title, x-axis label and the colorbar caption
-    plt.title(scale.name)
+    plt.title(f"Enhancement scale: {scale.name}")
 
     ax.set_xlabel("Color Index", color="black", labelpad=3.0)
 
