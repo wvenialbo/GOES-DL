3dd45625134c47af662a3f100c9488202cef28a2
wvenialbo <wvenialbo@gmail.com>
2025-04-28 07:06:42 -0300

    refactor(enhancement): update imports and remove EnhacementTable class

--- src/goesdl/enhancement/__init__.py
+++ src/goesdl/enhancement/__init__.py
@@ -5,20 +5,17 @@ It includes functions to load, parse, process, and save McIDAS and GMT
 enhancement color tables, as well as reverse and manipulate color maps.
 """
 
-from .catalogue import cmap
-from .colormap import CombinedColormap
+from .catalogue import cmap, get_scale
 from .palette import EnhacementPalette
 from .preview import show_colormap
 from .scale import EnhancementScale
 from .stretching import EnhacementStretching
-from .table import EnhacementTable
 
 __all__ = [
-    "CombinedColormap",
     "EnhacementPalette",
     "EnhancementScale",
     "EnhacementStretching",
-    "EnhacementTable",
     "show_colormap",
     "cmap",
+    "get_scale",
 ]
--- src/goesdl/enhancement/catalogue.py
+++ src/goesdl/enhancement/catalogue.py
@@ -1,6 +1,31 @@
 from .scale import EnhancementScale
 
 
+def get_scale(enhancement_name: str) -> EnhancementScale:
+    palette_name, stretching_name = (enhancement_name.split("/") + [""])[:2]
+
+    if palette_name not in cmap:
+        supported_cmaps = "'".join(cmap.keys())
+        raise ValueError(
+            f"'{palette_name}' is not a valid colour map name, supported values are: "
+            f"'{supported_cmaps}'"
+        )
+
+    if stretching_name and stretching_name not in smap:
+        supported_smaps = "'".join(smap.keys())
+        raise ValueError(
+            f"'{stretching_name}' is not a valid stretching table name, supported values are: "
+            f"'{supported_smaps}'"
+        )
+    else:
+        stretching_name = stretching_name or "K200-240"
+
+    palette = cmap[palette_name]
+    stretching = smap[stretching_name]
+
+    return EnhancementScale(palette, stretching)
+
+
 def _ircolor() -> EnhancementScale:
     # Define the properties of a stock palette
     key_points = (200.0, 240.0, 320.0)  # (180, 240, 330)
@@ -11,4 +36,7 @@ def _ircolor() -> EnhancementScale:
     return EnhancementScale.from_colormap(cmap_name, color_maps, key_points)
 
 
-cmap: dict[str, EnhancementScale] = {"IRCOLOR": _ircolor()}
+# cmap: dict[str, EnhancementScale] = {"IRCOLOR": _ircolor()}
+
+cmap: dict[str, int] = {}
+smap: dict[str, int] = {}
