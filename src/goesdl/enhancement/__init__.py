47a75167f671b32389751851b6392ac3b52883c5
wvenialbo <wvenialbo@gmail.com>
2025-04-29 13:22:26 -0300

    refactor(enhancement): remove cmap from subpackage exports

--- src/goesdl/enhancement/__init__.py
+++ src/goesdl/enhancement/__init__.py
@@ -5,7 +5,7 @@ It includes functions to load, parse, process, and save McIDAS and GMT
 enhancement color tables, as well as reverse and manipulate color maps.
 """
 
-from .catalogue import cmap, get_scale
+from .catalogue import get_scale
 from .palette import EnhacementPalette
 from .preview import preview_colormap
 from .scale import EnhancementScale
@@ -18,7 +18,6 @@ __all__ = [
     "EnhancementScale",
     "EnhacementStretching",
     "preview_colormap",
-    "cmap",
     "get_scale",
     "get_stmap",
 ]
