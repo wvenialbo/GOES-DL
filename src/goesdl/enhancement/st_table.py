d3d7d64a2dcefd5c42d381f059a62f2ca42b5313
wvenialbo <wvenialbo@gmail.com>
2025-04-30 15:06:11 -0300

    refactor(enhancement): update the data line format

--- src/goesdl/enhancement/st_table.py
+++ src/goesdl/enhancement/st_table.py
@@ -61,7 +61,7 @@ class st_utility:
     def _create_stretching_table(
         lines: list[str], table: StretchingTable
     ) -> None:
-        lines.extend(f"{x:>8}{y:>9}" for x, y in table)
+        lines.extend(f"{x:>10.2f}{y:>7d}" for x, y in table)
 
     @classmethod
     def parse_table(cls, lines: list[str]) -> tuple[StretchingTable, str]:
