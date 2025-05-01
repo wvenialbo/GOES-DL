69fd893cb360fef9519130d47faab83203414100
wvenialbo <wvenialbo@gmail.com>
2025-05-01 16:53:02 -0300

    refactor(plotting): rename preview.py module to colormaps.py

--- src/goesdl/plotting/__init__.py
+++ src/goesdl/plotting/__init__.py
@@ -1,4 +1,4 @@
-from .preview import plot_brightness_profile, preview_colormap
+from .colormaps import plot_brightness_profile, preview_colormap
 from .single_plot import GSPlot, GSPlotParameter
 
 __all__ = [
