d5b459ae8c7d050d42f2ff437f7dd5782cde3fb6
wvenialbo <wvenialbo@gmail.com>
2025-05-03 16:30:52 -0300

    refactor(enhancement): move colour model conversion to clr_utility

--- src/goesdl/enhancement/clr_table.py
+++ src/goesdl/enhancement/clr_table.py
@@ -1,8 +1,35 @@
-from .constants import BRG_MAX, CLR_MAX
-from .shared import ColorTable, ColorValueList, KeypointList, ValueTable
+from colorsys import hsv_to_rgb
+
+from .constants import (
+    BRG_MAX,
+    CLR_MAX,
+    CMYK_MAX,
+    HSV_MAX,
+    HUE_MAX,
+)
+from .shared import (
+    ColorTable,
+    ColorValueList,
+    KeypointList,
+    RGBValue,
+    ValueTable,
+)
 
 
 class clr_utility:
+
+    @staticmethod
+    def _cmyk_to_rgb(c: float, m: float, y: float, k: float) -> RGBValue:
+        b = 1.0 - k / CMYK_MAX
+        return (
+            (1.0 - c / CMYK_MAX) * b,
+            (1.0 - m / CMYK_MAX) * b,
+            (1.0 - y / CMYK_MAX) * b,
+        )
+
+    @staticmethod
+    def _hsv_to_rgb(h: float, s: float, v: float) -> RGBValue:
+        return hsv_to_rgb(h / HUE_MAX, s / HSV_MAX, v / HSV_MAX)
 
     @classmethod
     def _make_color_table(cls, values: ValueTable) -> ColorTable:
