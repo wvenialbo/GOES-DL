6a97c407468c4c6843aa62b6c868da95e972611b
wvenialbo <wvenialbo@gmail.com>
2025-05-02 21:40:02 -0300

    feat(plotting): export preview_stretching from plotting subpackage

--- src/goesdl/plotting/__init__.py
+++ src/goesdl/plotting/__init__.py
@@ -2,13 +2,15 @@ from .colormaps import (
     plot_brightness_profile,
     plot_color_profile,
     preview_colormap,
+    preview_stretching,
 )
 from .single_plot import GSPlot, GSPlotParameter
 
 __all__ = [
     "GSPlot",
     "GSPlotParameter",
-    "plot_brightness_profile",
     "plot_color_profile",
+    "plot_brightness_profile",
+    "preview_stretching",
     "preview_colormap",
 ]
