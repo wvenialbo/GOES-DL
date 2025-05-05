24a522bdcb4f062f3ca1d4bcc3c6baf41935f2f0
wvenialbo <wvenialbo@gmail.com>
2025-05-05 03:47:14 -0300

    refactor(enhancement): add load_cpt
    Add EnhancementScale.load_cpt to expose EnhacementPalette functionality.

--- src/goesdl/enhancement/scale.py
+++ src/goesdl/enhancement/scale.py
@@ -132,6 +132,13 @@ class EnhancementScale:
         cpal = EnhacementPalette.load(path, ncolors, invert)
         return cls(cpal)
 
+    @classmethod
+    def load_cpt(
+        cls, path: str | Path, ncolors: int = 256, invert: bool = False
+    ) -> "EnhancementScale":
+        cpal = EnhacementPalette.load_cpt(path, ncolors, invert)
+        return cls(cpal)
+
     def load_stretching(self, path: str | Path) -> None:
         """
         Create an EnhacementStretching instance from a file.
