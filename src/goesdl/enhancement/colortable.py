ba46104cafcb0e3338d287fc1d588b07820fc38d
wvenialbo <wvenialbo@gmail.com>
2025-05-04 23:52:07 -0300

    refactor(enhancement): update ColormapTable
    Update ColormapTable to accommodate last changes.

--- src/goesdl/enhancement/colortable.py
+++ src/goesdl/enhancement/colortable.py
@@ -192,70 +192,92 @@ class EUColorTable(_ColorTable):
 
 class ColormapTable(_ColorTable):
     """
-    Represent a colour enhancement colour table.
+    Represent a enhancement colour table.
 
     This class provides methods to load, parse, and process McIDAS and
-    GMT enhancement colour tables, creating colour segments for
-    Matplotlib colormaps.
+    GMT enhancement colour table files.
+
+    Notes
+    -----
+    The Man computer Interactive Data Access System (McIDAS) is a
+    research quality suite of applications used for decoding,
+    analyzing, and displaying meteorological data developed by the
+    University of Wisconsin-Madison Space Science and Engineering
+    Center (UWisc/SSEC).
+
+    - https://www.ssec.wisc.edu/mcidas/
+    - https://www.unidata.ucar.edu/software/mcidas/
+
+    The Generic Mapping Tools (GMT) is an open-source collection of
+    tools for manipulating geographic and Cartesian data sets and
+    producing PostScript illustrations ranging from simple x-y plots
+    through maps to complex 3D perspective views.
+
+    - https://www.generic-mapping-tools.org/
     """
 
     def __init__(self, path: str | Path, ncolors: int = 256) -> None:
+        color_table, stock_table, domain, name = self._from_text_file(path)
 
-        color_table, name = self._from_file(path)
+        color_list = self._make_color_list(color_table)
 
-        listed_colors = self._make_color_list(color_table)
+        name = name or Path(path).stem
 
-        super().__init__(name, listed_colors, ncolors)
+        super().__init__(name, color_list, ncolors)
+
+        self.set_domain(domain)
+
+        under, over, bad = (entry[1:] for entry in stock_table)
+
+        self.set_stock_colors(under, over, bad)
 
     @classmethod
-    def _from_file(cls, path: str | Path) -> tuple[ColorTable, str]:
-        """
-        Load a McIDAS or GMT enhancement colour table specification.
+    def _from_file(
+        cls, path: str | Path
+    ) -> tuple[ColorTable, ColorTable, DomainData, str]:
+        with open(path, "rb") as file:
+            data = file.read()
 
-        Parse a McIDAS enhancement utility table (EU TABLE) or GMT
-        colour palette table (CPT TABLE) text file and create colour
-        dictionary for a Matplotlib colormap.
+        if et_utility.is_et_table(data[:4]) and et_utility.has_expected_size(
+            data
+        ):
+            return cls._parse_binary_file(data)
 
-        Parameters
-        ----------
-        path : str or Path
-            Path to the colour table specification text file.
-
-        Returns
-        -------
-        tuple[ColorTable, str]
-            Tuple of colour table and name values.
-
-        Notes
-        -----
-        The Man computer Interactive Data Access System (McIDAS) is a
-        research quality suite of applications used for decoding,
-        analyzing, and displaying meteorological data developed by the
-        University of Wisconsin-Madison Space Science and Engineering
-        Center (UWisc/SSEC).
-
-        - https://www.ssec.wisc.edu/mcidas/
-        - https://www.unidata.ucar.edu/software/mcidas/
-
-        The Generic Mapping Tools (GMT) is an open-source collection of
-        tools for manipulating geographic and Cartesian data sets and
-        producing PostScript illustrations ranging from simple x-y plots
-        through maps to complex 3D perspective views.
-
-        - https://www.generic-mapping-tools.org/
-        """
         with open(path, "r", encoding="utf-8") as file:
             lines = file.readlines()
 
-        color_table, name = cls._parse_text_file(lines)
-
-        return color_table, name
+        return cls._parse_text_file(lines)
 
     @staticmethod
-    def _parse_text_file(lines: list[str]) -> tuple[ColorTable, str]:
+    def _parse_binary_file(
+        data: bytes,
+    ) -> tuple[ColorTable, ColorTable, DomainData, str]:
+        try:
+            # Try parse a .ET file
+            return *et_utility.parse_et_table(data), ""
+
+        except (ValueError, IndexError, TypeError) as error:
+            raise ValueError(INVALID_ET_FILE) from error
+
+    @staticmethod
+    def _parse_text_file(
+        lines: list[str],
+    ) -> tuple[ColorTable, ColorTable, DomainData, str]:
         # Try parse a EU file first (if EU file detected)
         if eu_utility.is_eu_table(lines[0]):
-            return eu_utility.parse_eu_table(lines)
+            try:
+                # Try parse a .EU file
+                return eu_utility.parse_eu_table(lines)
+
+            except (ValueError, IndexError, TypeError) as error:
+                raise ValueError(INVALID_EU_FILE) from error
 
         # Try parse a CPT file
-        return cpt_utility.parse_cpt_table(lines)
+        try:
+            # Try parse a .CPT file
+            return *cpt_utility.parse_cpt_table(lines), ""
+
+        except (ValueError, IndexError, TypeError) as error:
+            raise ValueError(
+                "Invalid GMT colour palette table (.CPT) file"
+            ) from error
