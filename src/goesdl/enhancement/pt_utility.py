ccfa05b5d8e693eb26b0a8403a4d4db257cecc31
wvenialbo <wvenialbo@gmail.com>
2025-05-10 02:03:23 -0300

    fix(enhancement): update pt_utility to properly create a listed colourmap

--- src/goesdl/enhancement/pt_utility.py
+++ src/goesdl/enhancement/pt_utility.py
@@ -1,12 +1,14 @@
 from typing import cast
 
+from .clr_utility import clr_utility
 from .shared import ColorList, DiscreteColorList, DomainData, RGBValue
 
 
-class pt_utility:
+class pt_utility(clr_utility):
 
-    @staticmethod
+    @classmethod
     def parse_plain_text(
+        cls,
         lines: list[str],
     ) -> tuple[ColorList, DomainData]:
         color_list: DiscreteColorList = []
@@ -16,10 +18,14 @@ class pt_utility:
                 c / 255.0 for c in map(float, [a.strip(",") for a in ls[:3]])
             )
             color_list.append(cast(RGBValue, rgb))
+
         vmax = len(color_list) - 1
         color_table: ColorList = [
             (i / vmax, *rgb) for i, rgb in enumerate(color_list)
         ]
+
+        color_table = cls._combine_lists(color_table, color_table)
+
         domain = 0.0, float(vmax)
 
         return color_table, domain
