07888eb332f25bc8eb9a08867802366ea9c8d6e5
wvenialbo <wvenialbo@gmail.com>
2025-05-17 06:24:37 -0300

    refactor(fileio): export FileRepository from fileio

--- src/goesdl/fileio/__init__.py
+++ src/goesdl/fileio/__init__.py
@@ -1,4 +1,10 @@
 from .inventory import DatasetInventory
 from .metadata import load_metadata, save_metadata
+from .repository import FileRepository
 
-__all__ = ["DatasetInventory", "load_metadata", "save_metadata"]
+__all__ = [
+    "DatasetInventory",
+    "FileRepository",
+    "load_metadata",
+    "save_metadata",
+]
